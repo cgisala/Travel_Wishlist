cb78fbe76b441c80a84ead3085c1b9ee0cc48e07
chris <cgisala@msn.com>
2020-04-06 00:11:17 -0500

    imporved appearance and added Unit test

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/chris/MEGAsync/School/2020/Spring 2020/Software Development Capstone/Labs/django_wishlist/wishlist/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
